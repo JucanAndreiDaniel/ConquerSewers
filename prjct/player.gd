ea4304bed3be7397eb6f0b38c1389bcf6fd88179
BogdanDragos00 <dragos.bogdan00@e-uvt.ro>
2022-05-15 14:42:09 +0300

    facing_direction

--- prjct/player.gd
+++ prjct/player.gd
@@ -7,6 +7,7 @@ const MAXSPEED = 200
 const JUMPFORCE = 150
 
 var motion = Vector2()
+var facing_right = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,15 +20,22 @@ func _physics_process(delta):
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED
 
+	if facing_right == true:
+		$Sprite.scale.x = 1
+	else:
+		$Sprite.scale.x = -1
+
 	if Input.is_action_pressed("right"):
 		motion.x = MAXSPEED
+		facing_right = true
 	elif Input.is_action_pressed("left"):
 		motion.x = -MAXSPEED
+		facing_right = false
 	else:
 		motion.x = 0
 
 	if is_on_floor():
-		if Input.is_action_pressed("jump"):
+		if Input.is_action_just_pressed("jump"):
 			motion.y = -JUMPFORCE
 	
 	motion = move_and_slide(motion, UP)
